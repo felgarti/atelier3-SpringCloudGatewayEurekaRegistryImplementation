fce0ab9bc7e91c9f933f0677a50d26014b9e1089
felgarti <elgartifatima@gmail.com>
2022-10-31 20:40:57 +0100

    customer-service

---
+++ src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,30 @@
+package org.sid.customerservice;
+
+import org.sid.customerservice.entities.Customer;
+import org.sid.customerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+@Bean
+	CommandLineRunner start(CustomerRepository customerRepository)
+{
+	return args -> {
+		customerRepository.save(new Customer(null,"Mohamed" , "med@gmail.com")) ;
+		customerRepository.save(new Customer(null,"Yassine" , "Yassine@gmail.com")) ;
+		customerRepository.save(new Customer(null,"Imane" , "Imane@gmail.com")) ;
+		customerRepository.findAll().forEach(c->
+		{
+			System.out.println(c.toString());
+		});
+	} ;
+}
+}
